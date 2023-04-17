1d8e8cb5b184c49b74f204ad47b3d3f986b9c95f
DongwookJeong <nolhoon211@gmail.com>
2023-04-17 19:36:28 +0900

    입력 기능 부분

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,12 +8,12 @@ const App = () => {
     {
     id: 1,
     text: '리액트의 기초 알아보기',
-    checked: true,
+    checked: false,
   },
   {
     id: 2,
     text: '컴포넌트 스타일링해 보기',
-    checked: true,
+    checked: false,
   },
   {
     id: 3,
